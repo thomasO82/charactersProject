d04f15ecdcbe2f04efb7e6825c328ea5e508981c
thomasO82 <thomas.oliver@ri7.fr>
2023-04-01 16:05:06 +0200

    tralalla

--- models/Character.js
+++ models/Character.js
@@ -1,9 +1,10 @@
-class Character{
-    constructor(_name,_strong,_intel,_img){
+class Character {
+    constructor(_name, _strong, _intel, _img) {
         this.id = Math.floor(Math.random() * Date.now())
         this.name = _name
         this.strong = _strong
         this.intel = _intel
         this.img = "./assets/img/testImg.png"
+        //hello syl 
     }
 }
